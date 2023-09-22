ef47399e5251eefa2d80a9bc96e763b078ca5c86
QasimSandhu <qasimsandhu.itians@gmail.com>
2023-09-22 13:39:37 +0500

    Initial commit

--- src/Components/News.js
+++ src/Components/News.js
@@ -8,15 +8,40 @@ export class News extends Component {
         super();
         this.state = {
             articles: [],
-            loading: true
-        }
+            loading: false,
+            page: 1,
+        };
     }
 
     async componentDidMount() {
-        let APiUrl = "https://newsapi.org/v2/everything?q=tesla&from=2023-08-22&sortBy=publishedAt&apiKey=91f50a86b7dd45e895aa659ad948a38b";
-        let ApiData = await fetch(APiUrl);
-        let parsedData = await ApiData.json();
-        this.setState({ parsedData: parsedData.articles })
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+
+        } else {
+            const APiUrl = "https://newsapi.org/v2/everything?q=tesla&from=2023-08-22&sortBy=publishedAt&apiKey=91f50a86b7dd45e895aa659ad948a38b&page=1&&pageSize=20";
+            const ApiData = await fetch(APiUrl);
+            const parsedData = await ApiData.json();
+            this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults })
+        }
+    }
+
+    handlePreClick = async () => {
+        const APiUrl = `https://newsapi.org/v2/everything?q=tesla&from=2023-08-22&sortBy=publishedAt&apiKey=91f50a86b7dd45e895aa659ad948a38b&page=${this.state.page - 1}&pageSize=20`;
+        const ApiData = await fetch(APiUrl);
+        const parsedData = await ApiData.json();
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles
+        })
+    }
+
+    handleNextClick = async () => {
+        const APiUrl = `https://newsapi.org/v2/everything?q=tesla&from=2023-08-22&sortBy=publishedAt&apiKey=91f50a86b7dd45e895aa659ad948a38b&page=${this.state.page + 1}&pageSize=20`;
+        const ApiData = await fetch(APiUrl);
+        const parsedData = await ApiData.json();
+        this.setState({
+            page: this.state.page + 1,
+            articles: parsedData.articles
+        })
     }
 
     render() {
@@ -29,6 +54,10 @@ export class News extends Component {
                             <NewsItem title={element.title ? element.title.slice(0, 30) : ""} description={element.description ? element.description.slice(0, 70) : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
                         </div>
                     })}
+                </div>
+                <div className="container d-flex justify-content-between">
+                    <button type="button" disabled={this.state.page <= 1} className="btn btn-dark" onClick={this.handlePreClick}>&larr; Previous</button>
+                    <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
                 </div>
             </div>
         )
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        let { title, description, imageUrl, newsUrl} = this.props;
+        let { title, description, imageUrl, newsUrl } = this.props;
         return (
             <div className='container my-3'>
-                <div className="card" style={{width: "18rem"}}>
-                    <img src={imageUrl ? imageUrl : "https://cmg-cmg-tv-10070-prod.cdn.arcpublishing.com/resizer/XfUA9tDsZY6qJO8j0Hz06TZ2hpM=/1440x810/filters:format(png):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/cmg/E4CZZ5DGAZF7VG7TXWWZIY4XKE.png"} className="card-img-top" alt="Failed to load images" />
+                <div className="card" style={{ width: "18rem" }}>
+                    <img src={imageUrl ? imageUrl : "https://i-invdn-com.investing.com/news/LYNXMPEB280W7_L.jpg"} className="card-img-top" alt="Failed to load images" />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
